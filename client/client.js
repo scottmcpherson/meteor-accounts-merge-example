f83d8c978e59cd7203d3e5f94144f2e360138295
Scott McPherson <scott_fl@me.com>
2014-05-29 08:11:58 -0400

    password support

--- client/client.js
+++ client/client.js
@@ -83,8 +83,74 @@ Template.signIn.events({
     });
     e.preventDefault();
   },
+  'click button#login-github': function (e) {
+    Meteor.signInWithGithub ({}, function (error, mergedUserId) {
+      if (error) {
+        console.log('error', error);
+      }
 
-  // Logout
+      // mergedUserId is set if a merge occured
+      if (mergedUserId) {
+        console.log('mergedUserId', mergedUserId);
+
+        // The source account (mergedUserId) has now been deleted, so now is
+        // your chance to deal with you application specific DB items to avoid
+        // ending up with orphans. You'd typically want to change owner on the
+        // items beloning to the deleted user, or simply delete them.
+        Meteor.call ('mergeItems', mergedUserId, function (error, result) {
+          if (error) {
+            console.log('error', error);
+          }
+          if (result) {
+            console.log('result', result);
+          }
+        });
+      }
+    });
+    e.preventDefault();
+  },
+  'submit #signup-password' : function(e, t){
+    e.preventDefault();
+    options = {};
+    // retrieve the input field values
+    var email = t.find('#email').value
+      , password = t.find('#password').value;
+
+    console.log("email: ", email);
+
+    options.email = email;
+    options.password = password;
+
+    // Trim and validate your fields here....
+
+    // If validation passes, supply the appropriate fields to the
+    // Meteor.loginWithPassword() function.
+    Meteor.createPasswordUser( options, function(err, result){
+      if (err) {
+        // The user might not have been found, or their passwword
+        // could be incorrect. Inform the user that their
+        // login attempt has failed.
+        console.log(err);
+      } else {
+        // The user has been logged in.
+        console.log(result);
+      }
+    });
+    return false;
+  },
+  'submit #login-password': function(e, t) {
+    var email = t.find('#email-login').value
+      , password = t.find('#password-login').value;
+
+      console.log("email: ", email);
+      console.log("password: ", password);
+
+    Meteor.loginWithPassword(email, password, function(error) {
+      if(error)
+        console.log(error);
+    })
+    return false;
+  },
   'click button#logout': function (e) {
     Meteor.logout(function (error) {
       if (error) {
@@ -94,3 +160,4 @@ Template.signIn.events({
     e.preventDefault();
   },
 });
+
